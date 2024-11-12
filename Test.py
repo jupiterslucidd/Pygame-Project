11a107f5bca6220b1731c1aafdd1d8cf200debd7
Rachel G <54511571+jupiterslucidd@users.noreply.github.com>
2024-11-12 12:47:40 -0500

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,6 +1,8 @@
 import pygame
 from pygame.locals import *
 import sys
+import random
+
 pygame.mixer.init()
 pygame.init()
 
@@ -37,7 +39,7 @@ def InstructionScreen():
     line1 = menuInstructions.render("All you need to do is to get to the end of each maze!", True, (255, 255, 255))
     line1Rect = line1.get_rect(center=(400, 120))
 
-    line2 = menuInstructions.render("There will be four levels of mazes which will increase in difficulty!", True,
+    line2 = menuInstructions.render("There will be two levels of mazes which will increase in difficulty!", True,
                                     (255, 255, 255))
     line2Rect = line2.get_rect(center=(400, 150))
 
@@ -83,12 +85,6 @@ def InstructionScreen():
     extras = extrasInstructions.render("Things you might need:", True, (255, 255, 255))
     extrasRect = extras.get_rect(center=(580, 280))
 
-    line8 = extrasInstructions.render("M- Flashlight", True, (255, 255, 255))
-    line8Rect = line8.get_rect(center=(590, 340))
-
-    line9 = extrasInstructions.render("N- Weapon", True, (255, 255, 255))
-    line9Rect = line9.get_rect(center=(590, 370))
-
     running = True
     while running:
         keys = pygame.key.get_pressed()
@@ -100,6 +96,9 @@ def InstructionScreen():
             if event.type == MOUSEBUTTONDOWN:
                 if menuBackRect.collidepoint(event.pos):
                     MenuScreen()
+            if event.type == MOUSEBUTTONDOWN:
+                if startRect.collidepoint(event.pos):
+                    level1()
         # change color of the "start" option to red when hovered over
         mouse_pos = pygame.mouse.get_pos()
         if startRect.collidepoint(mouse_pos):
@@ -133,9 +132,6 @@ def InstructionScreen():
         screen.blit(line6, line6Rect)
         screen.blit(line7, line7Rect)
 
-        screen.blit(extras, extrasRect)
-        screen.blit(line8, line8Rect)
-        screen.blit(line9, line9Rect)
         pygame.display.flip()
 
 def CreditsScreen():
@@ -201,6 +197,218 @@ def CreditsScreen():
         screen.blit(menuBackSurf, menuBackRect)
         pygame.display.flip()
 
+def DeathScreen():
+    screen = pygame.display.set_mode((800, 500))
+
+    # black and white background version
+    scaryBackground = pygame.image.load("BWscaryBackground1.jpeg")
+    scaryBackground = pygame.transform.scale(scaryBackground, (800, 500))
+
+    # "wasted" title
+    deadTitle = pygame.font.Font('pricedown bl.otf', 100)
+    deadTitleSurf = deadTitle.render("wasted", True, (255, 0, 0))
+
+    deadTitleRect = deadTitleSurf.get_rect()
+    deadTitleRect.center = (400, 140)
+
+    # "try again"
+    deadTryagain = pygame.font.Font('PixelifySans-VariableFont_wght.ttf', 30)
+    deadTryagainSurf = deadTryagain.render("Try Again", True, (255, 255, 255))
+
+    deadTryagainRect = deadTryagainSurf.get_rect()
+    deadTryagainRect.center = (400, 250)
+
+    # "exit"
+    deadExit = pygame.font.Font('PixelifySans-VariableFont_wght.ttf', 30)
+    deadExitSurf = deadExit.render("Exit", True, (255, 255, 255))
+
+    deadExitRect = deadExitSurf.get_rect()
+    deadExitRect.center = (400, 289)
+
+    running = True
+    while running:
+        keys = pygame.key.get_pressed()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit(0)
+            if keys[pygame.K_ESCAPE]:
+                sys.exit(0)
+            if event.type == MOUSEBUTTONDOWN:
+                if deadExitRect.collidepoint(event.pos):
+                    sys.exit(0)
+            if event.type == MOUSEBUTTONDOWN:
+                if deadTryagainRect.collidepoint(event.pos):
+                    level1()
+
+        mouse_pos = pygame.mouse.get_pos()
+
+        if deadTryagainRect.collidepoint(mouse_pos):
+            deadTryagainSurf = deadTryagain.render("Try Again", True, (255, 0, 0))
+        else:
+            deadTryagainSurf = deadTryagain.render("Try Again ", True, (255, 255, 255))
+
+        if deadExitRect.collidepoint(mouse_pos):
+            deadExitSurf = deadExit.render("Exit", True, (255, 0, 0))
+        else:
+            deadExitSurf = deadExit.render("Exit", True, (255, 255, 255))
+
+        screen.blit(scaryBackground, (0, 0))
+
+        screen.blit(deadTitleSurf, deadTitleRect)
+        screen.blit(deadTryagainSurf, deadTryagainRect)
+        screen.blit(deadExitSurf, deadExitRect)
+
+        pygame.display.flip()
+
+def Hooray():
+    screen = pygame.display.set_mode((800, 500))
+
+    # background image
+    scaryBackground = pygame.image.load("scaryBackground.jpeg")
+    scaryBackground = pygame.transform.scale(scaryBackground, (800, 500))
+
+    #"Hooray" Sound
+    hiphipHooray = pygame.mixer.Sound("FNAFHooray.mp3")
+    sound_played = False
+
+    # "You Win!" title
+    winTitle = pygame.font.Font('PixelifySans-VariableFont_wght.ttf', 120)
+    winTitleSurf = winTitle.render("You Win!", True, (255, 255, 255))
+
+    winTitleRect = winTitleSurf.get_rect()
+    winTitleRect.center = (400, 140)
+
+    # "Back to Menu" option
+    menu = pygame.font.Font('PixelifySans-VariableFont_wght.ttf', 30)
+    menuSurf = menu.render("Back to menu", True, (255, 255, 255))
+
+    menuRect = menuSurf.get_rect()
+    menuRect.center = (400, 250)
+
+    running = True
+    while running:
+        keys = pygame.key.get_pressed()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit(0)
+            if keys[pygame.K_ESCAPE]:
+                sys.exit(0)
+            if event.type == MOUSEBUTTONDOWN:
+                if menuRect.collidepoint(event.pos):
+                    MenuScreen()  # Go back to menu when "Back to menu" is clicked
+
+        mouse_pos = pygame.mouse.get_pos()
+
+        if menuRect.collidepoint(mouse_pos):
+            menuSurf = menu.render("Back to menu", True, (255, 0, 0))
+        else:
+            menuSurf = menu.render("Back to menu", True, (255, 255, 255))
+
+        #plays hiphipHooray sound
+        if not sound_played:
+            hiphipHooray.play()
+            sound_played = True
+
+        # Render the background and win message
+        screen.blit(scaryBackground, (0, 0))
+        screen.blit(winTitleSurf, winTitleRect)
+        screen.blit(menuSurf, menuRect)
+
+        pygame.display.flip()  # Ensure the screen is updated
+
+def level1():
+    screen = pygame.display.set_mode((800, 500))
+
+    # Background image
+    scaryBackground = pygame.image.load("scaryBackground.jpeg")
+    scaryBackground = pygame.transform.scale(scaryBackground, (800, 500))
+
+    # "Maze" background along with image
+    easyMaze = pygame.image.load("easyMaze.png")
+    easyMaze = pygame.transform.scale(easyMaze, (600, 500))
+    easyMaze_rect = easyMaze.get_rect(topleft=(0, 0))
+
+    # "maze" mask
+    easyMaze_mask = pygame.mask.from_surface(easyMaze)
+
+    # "Player"
+    player = pygame.image.load("player.png")
+    player = pygame.transform.scale(player, (40, 50))
+    playerRect = player.get_rect()
+    playerRect.topleft = (0, 50)  # Initial player position
+    player_speed = 1
+    # "player" mask
+    player_mask = pygame.mask.from_surface(player)
+
+    # "Enemy"
+    enemy = pygame.image.load("bat.png")
+    enemy = pygame.transform.scale(enemy, (40, 50))
+    enemy_mask = pygame.mask.from_surface(enemy)
+    enemy_speed = 0.5
+
+    def spawn_enemy():
+        enemy_x = random.randint(easyMaze_rect.left, easyMaze_rect.right - enemy.get_width())
+        enemy_y = random.randint(easyMaze_rect.top, easyMaze_rect.bottom - enemy.get_height())
+        return pygame.Rect(enemy_x, enemy_y, enemy.get_width(), enemy.get_height())
+
+    enemyRect = spawn_enemy()
+
+    #Winner end
+    winZone = pygame.Rect(easyMaze_rect.right - 60, easyMaze_rect.bottom - 60, 50, 50)
+
+    # "Exit" option
+    menuExit = pygame.font.Font('PixelifySans-VariableFont_wght.ttf', 35)
+    menuExitSurf = menuExit.render("EXIT", True, (255, 255, 255))
+    exitRect = menuExitSurf.get_rect()
+    exitRect.center = (690, 400)
+
+    running = True
+    while running:
+        keys = pygame.key.get_pressed()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit(0)
+            # Exit by clicking "EXIT"
+            if event.type == MOUSEBUTTONDOWN:
+                if exitRect.collidepoint(event.pos):
+                    sys.exit(0)
+
+        # Check movement in each direction and apply if no collision
+        if keys[pygame.K_a]:  # Move left
+            playerRect.x -= player_speed
+        if keys[pygame.K_d]:  # Move right
+            playerRect.x += player_speed
+        if keys[pygame.K_w]:  # Move up
+            playerRect.y -= player_speed
+        if keys[pygame.K_s]:  # Move down
+            playerRect.y += player_speed
+
+        offset = (playerRect.x - easyMaze_rect.x, playerRect.y - easyMaze_rect.y)
+        if easyMaze_mask.overlap(player_mask, offset):
+            DeathScreen()
+
+        enemyRect.y += enemy_speed
+        if enemyRect.top > easyMaze_rect.bottom:
+            enemyRect = spawn_enemy()
+
+        # death by enemy
+        offset_enemy = (enemyRect.x - playerRect.x, enemyRect.y - playerRect.y)
+        if player_mask.overlap(enemy_mask, offset_enemy):
+            DeathScreen()
+
+        # when player reaches end of maze
+        if playerRect.colliderect(winZone):
+            Hooray()
+
+        #Display
+        screen.blit(scaryBackground, (0, 0))
+        screen.blit(player, playerRect.topleft)
+        screen.blit(enemy, enemyRect.topleft)
+        screen.blit(easyMaze, easyMaze_rect.topleft)
+        screen.blit(menuExitSurf, exitRect)
+
+        pygame.display.flip()
+
 def MenuScreen():
     screen = pygame.display.set_mode((800, 500))
 
@@ -260,6 +468,7 @@ def MenuScreen():
                 if startRect.collidepoint(event.pos):
                     spookySound.play()
                     pygame.time.delay(2000) #Waits for sound to play then sends you to the next screen!
+                    level1()
 
             # clapping sound plays when you click start
             clappingSound = pygame.mixer.Sound('clappingSound.mp3')
@@ -267,6 +476,7 @@ def MenuScreen():
                 if creditsRect.collidepoint(event.pos):
                     clappingSound.play()
                     clappingSound.fadeout(2500) # fading out the sound
+                    level1()
 
             if event.type == MOUSEBUTTONDOWN:
                 if menuInstructionsRect.collidepoint(event.pos):
@@ -304,66 +514,6 @@ def MenuScreen():
         screen.blit(menuInstructionsSurf, menuInstructionsRect)
         screen.blit(menuCreditsSurf, creditsRect)
         screen.blit(menuExitSurf, exitRect)
-
-        pygame.display.flip()
-
-def DeathScreen():
-    screen = pygame.display.set_mode((800, 500))
-
-    # black and white background version
-    scaryBackground = pygame.image.load("BWscaryBackground1.jpeg")
-    scaryBackground = pygame.transform.scale(scaryBackground, (800, 500))
-
-    # "wasted" title
-    deadTitle = pygame.font.Font('pricedown bl.otf', 100)
-    deadTitleSurf = deadTitle.render("wasted", True, (255, 0, 0))
-
-    deadTitleRect = deadTitleSurf.get_rect()
-    deadTitleRect.center = (400, 140)
-
-    # "try again"
-    deadTryagain = pygame.font.Font('PixelifySans-VariableFont_wght.ttf', 30)
-    deadTryagainSurf = deadTryagain.render("Try Again", True, (255, 255, 255))
-
-    deadTryagainRect = deadTryagainSurf.get_rect()
-    deadTryagainRect.center = (400, 250)
-
-    # "exit"
-    deadExit = pygame.font.Font('PixelifySans-VariableFont_wght.ttf', 30)
-    deadExitSurf = deadExit.render("Exit", True, (255, 255, 255))
-
-    deadExitRect = deadExitSurf.get_rect()
-    deadExitRect.center = (400, 289)
-
-    running = True
-    while running:
-        keys = pygame.key.get_pressed()
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                sys.exit(0)
-            if keys[pygame.K_ESCAPE]:
-                sys.exit(0)
-            if event.type == MOUSEBUTTONDOWN:
-                if deadExitRect.collidepoint(event.pos):
-                    sys.exit(0)
-
-        mouse_pos = pygame.mouse.get_pos()
-
-        if deadTryagainRect.collidepoint(mouse_pos):
-            deadTryagainSurf = deadTryagain.render("Try Again", True, (255, 0, 0))
-        else:
-            deadTryagainSurf = deadTryagain.render("Try Again ", True, (255, 255, 255))
-
-        if deadExitRect.collidepoint(mouse_pos):
-            deadExitSurf = deadExit.render("Exit", True, (255, 0, 0))
-        else:
-            deadExitSurf = deadExit.render("Exit", True, (255, 255, 255))
-
-        screen.blit(scaryBackground, (0, 0))
-
-        screen.blit(deadTitleSurf, deadTitleRect)
-        screen.blit(deadTryagainSurf, deadTryagainRect)
-        screen.blit(deadExitSurf, deadExitRect)
 
         pygame.display.flip()
 
